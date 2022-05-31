d9396718da52d05090aa8c83f30bab608c7db3cb
Rawan Khaled <67297151+rere-kh1999@users.noreply.github.com>
2022-05-31 03:48:00 +0200

    Update gui.py
    Co-Authored-By: Shrouk-hub <73853361+Shrouk-hub@users.noreply.github.com>
    Co-Authored-By: Nouran Ahmed <52378608+nouraan-ahmed@users.noreply.github.com>
    Co-Authored-By: Renad Mostafa <74263134+renad-mostafa99@users.noreply.github.com>
    Co-Authored-By: shimaa88 <73905180+shimaa88@users.noreply.github.com>

--- gui.py
+++ gui.py
@@ -10,6 +10,30 @@ from PyQt5 import QtGui
 from PyQt5.QtCore import QRect
 from functools import reduce
 from sqlalchemy import true
+import algorithm_csp
+
+# @ <component>: <usage>
+
+# @ stderr: reporting errors
+# @ stdin: receiving input
+from sys import stderr, stdin
+
+# @ product: creation of the variables' domains
+# @ permutations: determine the satisfiability of an operation
+from itertools import product, permutations
+
+# @ reduce: determine the result of an operation
+from functools import reduce
+
+# @ seed: seed the pseudorandom number generator
+# @ random, shuffle, randint, choice: generate a random kenken puzzle
+from random import seed, random, shuffle, randint, choice
+
+# @ time: benchmarking
+from time import time
+
+# @ writer: output benchmarking data in a csv format
+from csv import writer
 
 COLOR_A = '#0FC'
 COLOR_ERR = '#F00'
@@ -103,7 +127,26 @@ def generate(size):
         cliques[-1] = (tuple(cliques[-1]), operator, int(target))
 
     return size, cliques
+def conflicting(A, a, B, b):
+    """
+    Evaluates to true if:
+      * there exists mA so that ma is a member of A and
+      * there exists mb so that mb is a member of B and
+      * RowXorCol(mA, mB) evaluates to true and
+      * the value of mA in 'assignment' a is equal to
+        the value of mb in 'assignment' b
+    """
+    for i in range(len(A)):
+        for j in range(len(B)):
+            mA = A[i]
+            mB = B[j]
 
+            ma = a[i]
+            mb = b[j]
+            if RowXorCol(mA, mB) and ma == mb:
+                return True
+
+    return False
 
 def validate(cliques):
     for member in cliques:
@@ -113,6 +156,280 @@ def validate(cliques):
 
     return True
 
+def RowXorCol(xy1, xy2):
+    """
+    Evaluates to true if the given positions are in the same row / column
+    but are in different columns / rows
+    """
+    return (xy1[0] == xy2[0]) != (xy1[1] == xy2[1])
+def conflicting(A, a, B, b):
+    """
+    Evaluates to true if:
+      * there exists mA so that ma is a member of A and
+      * there exists mb so that mb is a member of B and
+      * RowXorCol(mA, mB) evaluates to true and
+      * the value of mA in 'assignment' a is equal to
+        the value of mb in 'assignment' b
+    """
+    for i in range(len(A)):
+        for j in range(len(B)):
+            mA = A[i]
+            mB = B[j]
+
+            ma = a[i]
+            mb = b[j]
+            if RowXorCol(mA, mB) and ma == mb:
+                return True
+
+    return False
+
+def gdomains(size, cliques):
+    """
+    @ https://docs.python.org/2/library/itertools.html
+    @ product('ABCD', repeat=2) = [AA AB AC AD BA BB BC BD CA CB CC CD DA DB DC DD]
+    For every clique in cliques:
+        * Initialize the domain of each variable to contain every product
+        of the set [1...board-size] that are of length 'clique-size'.
+        For example:
+            board-size = 3 and clique-size = 2
+            products = [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]
+        * Discard any value (assignment of the members of the clique) that:
+        * is resulting in the members of the clique 'conflicting' with each other
+        * does not 'satisfy' the given operation
+    """
+    domains = {}
+    for clique in cliques:
+        members, operator, target = clique
+
+        domains[members] = list(product(range(1, size + 1), repeat=len(members)))
+
+        qualifies = lambda values: not conflicting(members, values, members, values) and satisfies(values, operation(operator), target)
+
+        domains[members] = list(filter(qualifies, domains[members]))
+
+    return domains
+def satisfies(values, operation, target):
+    """
+    Evaluates to true if the result of applying the operation
+    on a permutation of the given values is equal to the specified target
+    """
+    for p in permutations(values):
+        if reduce(operation, p) == target:
+            return True
+
+    return False
+def gneighbors(cliques):
+    """
+    Determine the neighbors of each variable for the given puzzle
+        For every clique in cliques
+        * Initialize its neighborhood as empty
+        * For every clique in cliques other than the clique at hand,
+            if they are probable to 'conflict' they are considered neighbors
+    """
+    neighbors = {}
+    for members, _, _ in cliques:
+        neighbors[members] = []
+
+    for A, _, _ in cliques:
+        for B, _, _ in cliques:
+            if A != B and B not in neighbors[A]:
+                if conflicting(A, [-1] * len(A), B, [-1] * len(B)):
+                    neighbors[A].append(B)
+                    neighbors[B].append(A)
+
+    return neighbors
+
+
+
+class Kenken(algorithm_csp.CSP):
+
+    def __init__(self, size, cliques):
+        """
+        In my implementation, I consider the cliques themselves as variables.
+        A clique is of the format (((X1, Y1), ..., (XN, YN)), <operation>, <target>)
+        where
+            * (X1, Y1), ..., (XN, YN) are the members of the clique
+            * <operation> is either addition, subtraction, division or multiplication
+            * <target> is the value that the <operation> should produce
+              when applied on the members of the clique
+        """
+        #validate(size, cliques)
+        
+        variables = [members for members, _, _ in cliques]
+        
+        domains = gdomains(size, cliques)
+
+        neighbors = gneighbors(cliques)
+
+        algorithm_csp.CSP.__init__(self, variables, domains, neighbors, self.constraint)
+
+        self.size = size
+
+        # Used in benchmarking
+        self.checks = 0
+
+        # Used in displaying
+        self.padding = 0
+
+        self.meta = {}
+        for members, operator, target in cliques:
+            self.meta[members] = (operator, target)
+            self.padding = max(self.padding, len(str(target)))        
+
+    # def nconflicts(self, var, val, assignment):
+
+    # def assign(self, var, val, assignment):
+
+    # def unassign(self, var, assignment):
+
+    def constraint(self, A, a, B, b):
+        """
+        Any two variables satisfy the constraint if they are the same
+        or they are not 'conflicting' i.e. every member of variable A
+        which shares the same row or column with a member of variable B
+        must not have the same value assigned to it
+        """
+        self.checks += 1
+
+        return A == B or not conflicting(A, a, B, b)
+
+    def display(self, assignment):
+        """
+        Print the kenken puzzle in a format easily readable by a human
+        """
+        if assignment:
+            atomic = {}
+            for members in self.variables:
+                values = assignment.get(members)
+
+                if values:
+                    for i in range(len(members)):
+                        atomic[members[i]] = values[i]
+                else:
+                    for member in members:
+                        atomic[member] = None
+        else:
+            atomic = {member:None for members in self.variables for member in members}
+
+        atomic = sorted(atomic.items(), key=lambda item: item[0][1] * self.size + item[0][0])
+
+        padding = lambda c, offset: (c * (self.padding + 2 - offset))
+
+        embrace = lambda inner, beg, end: beg + inner + end
+
+        mentioned = set()
+
+        def meta(member):
+            for var, val in self.meta.items():
+                if member in var and var not in mentioned:
+                    mentioned.add(var)
+                    return str(val[1]) + " " + (val[0] if val[0] != "." else " ")
+
+            return ""
+
+        fit = lambda word: padding(" ", len(word)) + word + padding(" ", 0)
+
+        cpadding = embrace(2 * padding(" ", 0), "|", "") * self.size + "|"
+
+        def show(row):
+
+            rpadding = "".join(["|" + fit(meta(item[0])) for item in row]) + "|"
+
+            data = "".join(["|" + fit(str(item[1] if item[1] else "")) for item in row]) + "|"
+
+            print(rpadding, data, cpadding, sep="\n")
+
+        rpadding = embrace(2 * padding("-", 0), "+", "") * self.size + "+"
+
+        print(rpadding)
+        for i in range(1, self.size + 1):
+
+            show(list(filter(lambda item: item[0][1] == i, atomic)))
+
+            print(rpadding)
+
+    def info(self):
+        """
+        Print debugging info
+        """
+
+        print("\nVariables:")
+        for var in self.variables:
+            print(var)
+
+        print("\nDomains:")
+        for var in self.variables:
+            print("domains[", var, "] =", self.domains[var])
+
+        print("\nNeighbors:")
+        for var in self.variables:
+            print("neighbors[", var, "] =", self.neighbors[var])
+
+def benchmark(kenken, algorithm):
+        """
+        Used in order to benchmark the given algorithm in terms of
+          * The number of nodes it visits
+          * The number of constraint checks it performs
+          * The number of assignments it performs
+          * The completion time
+        """
+        kenken.checks = kenken.nassigns = 0
+
+        dt = time()
+
+        assignment = algorithm(kenken)
+
+        dt = time() - dt
+
+        return assignment, (kenken.checks, kenken.nassigns, dt)
+
+def gather(iterations, out):
+    """
+    Benchmark each one of the following algorithms for various kenken puzzles
+      * For every one of the following algorithms
+       * For every possible size of a kenken board
+         * Create 'iterations' random kenken puzzles of the current size
+           and evaluate the algorithm on each one of them in order to get
+           statistically sound data. Then calculate the average evaluation
+           of the algorithm for the current size.
+      * Save the results into a csv file
+    """
+    bt         = lambda ken: algorithm_csp.backtracking_search(ken)
+    bt_mrv     = lambda ken: algorithm_csp.backtracking_search(ken, select_unassigned_variable=algorithm_csp.mrv)
+    fc         = lambda ken: algorithm_csp.backtracking_search(ken, inference=algorithm_csp.forward_checking)
+    fc_mrv     = lambda ken: algorithm_csp.backtracking_search(ken, inference=algorithm_csp.forward_checking, select_unassigned_variable=algorithm_csp.mrv)
+    mac        = lambda ken: algorithm_csp.backtracking_search(ken, inference=algorithm_csp.mac)
+    mconflicts = lambda ken: algorithm_csp.min_conflicts(ken)
+    algorithms = {
+        "BT": bt,
+        "BT+MRV": bt_mrv,
+        "FC": fc,
+        "FC+MRV": fc_mrv,
+        "MAC": mac,
+        "MIN_CONFLICTS": mconflicts
+    }
+
+    with open(out, "w+") as file:
+
+        out = writer(file)
+
+        out.writerow(["Algorithm", "Size", "Result", "Constraint checks", "Assignments", "Completion time"])
+
+        for name, algorithm in algorithms.items():
+            for size in range(3, 10):
+                checks, assignments, dt = (0, 0, 0)
+                for iteration in range(1, iterations + 1):
+                    size, cliques = generate(size)
+
+                    assignment, data = benchmark(Kenken(size, cliques), algorithm)
+
+                    print("algorithm =",  name, "size =", size, "iteration =", iteration, "result =", "Success" if assignment else "Failure", file=stderr)
+
+                    checks      += data[0] / iterations
+                    assignments += data[1] / iterations
+                    dt          += data[2] / iterations
+                    
+                out.writerow([name, size, checks, assignments, dt])
 class AnotherWindow(QMainWindow):
     """
     This "window" is a QWidget. If it has no parent, it
@@ -122,9 +439,13 @@ class AnotherWindow(QMainWindow):
     def _init_(self):
         super()._init_()
         top = 100
+        siz=0
         left = 100
         width = 800
         height = 800
+        self.cliques = []
+        self.size=0
+    
         self.setGeometry(left, top, width, height)
         self.setWindowTitle("hello")
         layout = QVBoxLayout()
@@ -150,15 +471,24 @@ class AnotherWindow(QMainWindow):
         button2.clicked.connect(self. Backtracking_with_forward_checking)
         button3.clicked.connect(self.Backtracking_with_arc_consistency)
         button4.clicked.connect(self.Close)
-
+    
     def Backtracking(self):
         print("hello")
+        ken = Kenken(self.size, self.cliques)
+        assignment = algorithm_csp.backtracking_search(ken)
+       
+        
+        print(assignment)
 
     def Backtracking_with_forward_checking(self):
-            print("h")
+        ken = Kenken(self.size, self.cliques)
+        assignment = algorithm_csp.backtracking_search(ken, inference=algorithm_csp.forward_checking)
+        print(assignment)
 
     def Backtracking_with_arc_consistency(self):
-         print("hello")
+        ken = Kenken(self.size, self.cliques)
+        assignment = algorithm_csp.backtracking_search(ken, inference=algorithm_csp.mac)
+        print(assignment)
 
     def Close(self):
        sys.exit()
@@ -182,20 +512,21 @@ class AnotherWindow(QMainWindow):
             self.solve_button.setEnabled(True)
 
     def initUI(self, n):
+        self.siz=n
         qtRectangle = self.frameGeometry()
         centerPoint = QDesktopWidget().availableGeometry().center()
         qtRectangle.moveCenter(centerPoint)
         self.move(qtRectangle.topLeft())
-        size, cliques = generate(n)
-        print(cliques)
+        self.size, self.cliques = generate(n)
+        print(self.cliques)
         self.setWindowTitle('KENKEN PUZZLE')
-        valid=validate(cliques)
+        valid=validate(self.cliques)
         while(valid==False):    
-            size, cliques = generate(n)
-            valid = validate(cliques)
+            self.size, self.cliques = generate(n)
+            valid = validate(self.cliques)
   
 
-        for members in cliques:
+        for members in self.cliques:
             random_number = randint(0, 16777215)
             hex_number = str(hex(random_number))
             color = '#' + hex_number[2:]
@@ -283,10 +614,18 @@ if __name__ == '__main__':
    # App=QApplication(sys.argv)
     # window=Window()
     # sys.exit(App.exec())
-    app = QApplication(sys.argv)
-    w = Window()
-    w.show()
-    app.exec()
+  
+     #size, cliques = generate(list(stdin))
+
+     #ken = Kenken(size, cliques)
+
+     #assignment = algorithm_csp.backtracking_search(ken)
+
+     #ken.display(assignment)
+     app = QApplication(sys.argv)
+     w = Window()
+     w.show()
+     app.exec()
 
 
 
